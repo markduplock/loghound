f82cb1c68a1e0801a200620e6c54cd75fadfb358
Mark <149008372+markduplock@users.noreply.github.com>
2026-09-04 19:53:22 +0100

    Read file

--- loghound.py
+++ loghound.py
@@ -1,5 +1,21 @@
+import sys
+
+
+def read_file(filename: str) -> str:
+    with open(filename, "r") as file:
+        content = file.read()
+
+    return content
+
+
+def get_args() -> str:
+    f: str = sys.argv[1]
+    return f
+
+
 def main() -> None:
-    pass
+    filename: str = get_args()
+    print(read_file(filename))
 
 
 if __name__ == "__main__":
